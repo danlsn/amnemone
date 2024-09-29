8c6eca606dbeefd9067d96d576fb4abb6d0f9d75
Daniel Lawson <52593003+danlsn@users.noreply.github.com>
2024-09-29 17:26:05 +1000

    feat(data): add classes for cities and countries

--- src/amnemone/data/__init__.py
+++ src/amnemone/data/__init__.py
@@ -1,4 +1,4 @@
-from amnemone.data.countries_states_cities import cities, countries, regions, states, subregions
+from amnemone.data.countries_states_cities import Cities, Countries, regions, states, subregions
 from amnemone.data.words import an_array_of_english_words
 
-__all__ = ['an_array_of_english_words', 'cities', 'countries', 'regions', 'states', 'subregions']
+__all__ = ['an_array_of_english_words', 'Cities', 'Countries', 'regions', 'states', 'subregions']
