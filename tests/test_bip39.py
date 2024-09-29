a32e06534bee454d8091a4b335d22dd4553a8f9e
Daniel Lawson <52593003+danlsn@users.noreply.github.com>
2024-09-29 16:48:45 +1000

    tests(bip39): fix bip39 short tests

--- tests/test_bip39.py
+++ tests/test_bip39.py
@@ -1,10 +1,10 @@
-from amnemone.data.bip39 import Bip39
+from amnemone.data.bip39 import Bip39, Bip39Short
 
 
 def test_bip39_radix():
     mn = Bip39()
     assert mn.radix == 2048
-    mn_short = Bip39(short=True)
+    mn_short = Bip39Short()
     assert mn_short.radix == 2048
     mn_rev = Bip39(reversed=True)
     assert mn_rev.radix == 2048
@@ -13,7 +13,7 @@ def test_bip39_radix():
 def test_bip39_random():
     mn = Bip39()
     assert len(mn.random(5)) == 5
-    mn_short = Bip39(short=True)
+    mn_short = Bip39Short()
     assert len(mn_short.random(5)) == 5
     mn_rev = Bip39(reversed=True)
     assert len(mn_rev.random(5)) == 5
@@ -37,5 +37,5 @@ def test_bip39_from_hex():
     ]
     mn = Bip39()
     assert mn.from_hex(hex_input) == expected_words
-    mn_short = Bip39(short=True)
+    mn_short = Bip39Short()
     assert mn_short.from_hex(hex_input) == [w[:4] for w in expected_words]
