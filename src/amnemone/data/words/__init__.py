278cb18f137d28d8ef366a2cf30406cd7eda9d21
Daniel Lawson <52593003+danlsn@users.noreply.github.com>
2024-09-29 14:49:49 +1000

    fix(data): change relative import to use pathlib

--- src/amnemone/data/words/__init__.py
+++ src/amnemone/data/words/__init__.py
@@ -1,6 +1,7 @@
 import json
+from pathlib import Path
 
 
 def an_array_of_english_words():
-    with open('./an-array-of-english-words.json', 'r') as file:
+    with open(Path(__file__).parent / 'an-array-of-english-words.json', 'r') as file:
         return json.load(file)
